55385b6d5e198d7cb5b3ba997be3a481fe60dfc1
Kenneth Udovic <kennyudovic@gmail.com>
2024-01-15 19:28:08 -0500

    chore: update sjel

--- packages/ajel-core/src/sjel.ts
+++ packages/ajel-core/src/sjel.ts
@@ -1,10 +1,10 @@
-type AnyFunction = (...args: any[]) => any;
-
-export const sjel = <Result>(fn: AnyFunction) => (...args: any[]) => {
-  try {
-    const result = fn(...args);
-    return [result, undefined] as [result: Result, error?: undefined];
-  } catch (error) {
-    return [undefined, error] as [result: undefined, error: unknown];
-  }
-};
+export const sjel =
+  <Args extends any[], Result>(fn: (...args: Args) => Result) =>
+    (...args: Args) => {
+      try {
+        const result = fn(...args);
+        return [result, undefined] as [Result, undefined];
+      } catch (error) {
+        return [undefined, error] as [undefined, unknown];
+      }
+    };
